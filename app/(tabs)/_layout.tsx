481539e727200b60eeb52f80003ca6faa58677aa
Leonardo Rodrigues <49464793+leorodrigues311@users.noreply.github.com>
2024-10-21 11:28:20 -0300

    primeiro commit

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,48 @@
+import { Tabs } from 'expo-router';
+import React from 'react';
+
+import { TabBarIcon } from '@/components/navigation/TabBarIcon';
+import { Colors } from '@/constants/Colors';
+import { useColorScheme } from '@/hooks/useColorScheme';
+
+export default function TabLayout() {
+  const colorScheme = useColorScheme();
+
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+        headerShown: false,
+      }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Inicio',
+          tabBarIcon: ({ color, focused }) => (
+            <TabBarIcon name={focused ? 'create' : 'create-outline'} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="historico"
+        options={{
+          title: 'Historico',
+          tabBarIcon: ({ color, focused }) => (
+            <TabBarIcon name={focused ? 'timer' : 'timer-outline'} color={color} />
+          ),
+        }}
+      />
+
+      <Tabs.Screen
+        name="ajustes"
+        options={{
+          title: 'Ajustes',
+          tabBarIcon: ({ color, focused }) => (
+            <TabBarIcon name={focused ? 'cog' : 'cog-outline'} color={color} />
+          ),
+        }}
+      />
+
+    </Tabs>
+  );
+}
---
+++ components/Comanda.tsx
@@ -0,0 +1,25 @@
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { StyleSheet, Text} from 'react-native';
+import react from 'react'
+
+export function Comanda(){
+    return (
+        <Text>Teste</Text>
+    
+        
+    );
+
+}
+
+
+const styles = StyleSheet.create({
+  heading: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 6,
+  },
+  content: {
+    marginTop: 6,
+    marginLeft: 24,
+  },
+});
