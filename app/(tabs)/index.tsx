76fa44f81232661e07c8f63926226ca285f2dde6
Leonardo Rodrigues <49464793+leorodrigues311@users.noreply.github.com>
2024-11-04 17:05:45 -0300

    configurar rotas

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,53 @@
-import Routes from "../routes/Routes"
+import {Pressable, StyleSheet, Text } from 'react-native';
+import { Comanda } from '@/components/Comanda';
+import { TopBar } from '@/components/navigation/TopBar';
+import { ButtonFlutuante } from '@/components/ButtonFlutuante';
+import { SafeAreaView } from 'react-native';
+import { View, ScrollView} from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { router } from "expo-router";
 
-const App = () => {
+export default function HomeScreen() {
 
-    return (
 
-        <Routes/>
-    )
-
+  const botaoComanda = () => {
+    const params = new URLSearchParams();
+    params.set('name', 'Leo')
+    params.set('age', '19')
+    router.navigate(`../components/ComandaDetalhe?${params.toString()}`)
 }
 
-export default App;
+
+  return (
+    <SafeAreaView style={styles.container}>
+       <TopBar/>
+       {/* <TopBarDetalheComanda/> */}
+       <ScrollView contentContainerStyle={styles.scrollViewContent}>
+       <Pressable onPress={botaoComanda}>
+          <Comanda
+            numeroComanda={1}
+            nomeComanda='João da Silva'
+            valorTotal={134.21}
+            horaAbertura='10:42'
+            statusComanda= {true}
+          />
+        </Pressable>
+      </ScrollView>
+      <ButtonFlutuante/>  
+    </SafeAreaView>
+
+  );
+}
+
+const styles = StyleSheet.create({
+
+  container: {
+    flex:1
+
+  },
+
+  scrollViewContent: {
+    flexGrow: 1,
+  },
+
+});
