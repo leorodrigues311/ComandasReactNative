6fba4333ea785b859a2b25f7deb0c3314ad939e6
Leonardo Rodrigues <49464793+leorodrigues311@users.noreply.github.com>
2024-11-28 12:02:52 -0300

    ajustes na nova comanda

--- app/novaComanda.tsx
+++ app/novaComanda.tsx
@@ -1,17 +1,122 @@
 import Ionicons from '@expo/vector-icons/Ionicons';
-import { StyleSheet, Text, View,  Modal, TextInput, Button} from 'react-native';
+import { Modal, Text, TextInput, View, Button, Pressable, StyleSheet } from 'react-native';
 import React, { useState } from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { ScrollView } from 'react-native';
 import { useLocalSearchParams  } from 'expo-router';
 
 
+
+
+
 export default function novaComanda (){
 
+  const [modalNumeroComandaVisivel, setModalNumeroComanda] = useState(false)
+  const [inputNumeroComanda, setInputNumeroComanda] = useState('')
+
+  const [modalNomeComandaVisivel, setModalNomeComanda] = useState(false)
+  const [inputNomeComanda, setInputNomeComanda] = useState('')
+
+  const handleButtonPress = (buttonType: string) => {
+    if (buttonType === 'confirmaNumero') {
+      setModalNumeroComanda(false);
+      setTimeout(() => {setModalNomeComanda(true)}, 500);
+    } 
+
+    else if (buttonType === 'cancela') {
+      setInputNumeroComanda('')
+    }
+    
+    else if (buttonType === 'voltaEtapa'){
+      setModalNomeComanda(false);
+      setTimeout(() => {setModalNumeroComanda(true)}, 500);
+    }
+
+    else if (buttonType === 'finaliza'){
+      setModalNomeComanda(false)
+
+    }
+    
+   
+  };
 
     return (
         <SafeAreaView style={styles.viewPrincipal} >
-    
+            <View style={styles.container}>
+            <Button title="Abrir Modal" onPress={() => setModalNumeroComanda(true)} />
+
+
+            <Modal
+              visible={modalNumeroComandaVisivel}
+              animationType="slide"
+              transparent={true}
+              onRequestClose={() => setModalNumeroComanda(false)}
+            >
+
+              <View style={styles.modalBackground}>
+                <View style={styles.modalContainer}>
+                  <Text style={styles.modalTitle}>Número da comanda:</Text>
+
+
+                  <TextInput
+                    style={styles.input}
+                    value={inputNumeroComanda}
+                    onChangeText={setInputNumeroComanda}
+                    placeholder="Número"
+                  />
+
+                  <View style={styles.buttonsContainer}>
+                    <Pressable onPress={() => handleButtonPress('cancela')}>
+                      <Ionicons name="arrow-back-outline" size={40} color="white" ></Ionicons>
+                    </Pressable>
+
+                    <Pressable onPress={() => handleButtonPress('autoPreenche')}>
+                      <Ionicons name="sync-outline" size={40} color="white" ></Ionicons>
+                    </Pressable>
+
+                    <Pressable onPress={() => handleButtonPress('confirmaNumero')}>
+                      <Ionicons name="arrow-forward-outline" size={40} color="white" ></Ionicons>
+                    </Pressable>
+
+                  </View>
+                </View>
+              </View>
+            </Modal>
+
+            <Modal
+              visible={modalNomeComandaVisivel}
+              animationType="slide"
+              transparent={true}
+              onRequestClose={() => setModalNomeComanda(false)}
+            >
+
+              <View style={styles.modalBackground}>
+                <View style={styles.modalContainer}>
+                  <Text style={styles.modalTitle}>Nome do cliente:</Text>
+
+
+                  <TextInput
+                    style={styles.input}
+                    value={inputNomeComanda}
+                    onChangeText={setInputNomeComanda}
+                    placeholder="Nome"
+                  />
+
+                  <View style={styles.buttonsContainer}>
+                    <Pressable onPress={() => handleButtonPress('voltaEtapa')}>
+                      <Ionicons name="arrow-back-outline" size={40} color="white" ></Ionicons>
+                    </Pressable>
+
+                    <Pressable onPress={() => handleButtonPress('finaliza')}>
+                      <Ionicons name="checkmark-done-outline" size={40} color="white" ></Ionicons>
+                    </Pressable>
+
+                  </View>
+                </View>
+              </View>
+            </Modal>
+
+          </View>
         </SafeAreaView>  
     );
 
@@ -28,6 +133,47 @@ const styles = StyleSheet.create({
     flexDirection: 'column',
     flex:1,
     borderRadius:5
+  },
+
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20
+  },
+  modalBackground: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0,0,0,0.5)'
+  },
+  modalContainer: {
+    width: '80%',
+    padding: 20,
+    backgroundColor: '#363636',
+    borderRadius: 10,
+    alignItems: 'center'
+  },
+  modalTitle: {
+    fontSize: 18,
+    marginBottom: 20,
+    color:'white'
+  },
+  input: {
+    height: 55,
+    width: '100%',
+    borderColor: '#4F4F4F',
+    borderWidth: 1,
+    borderRadius: 5,
+    paddingHorizontal: 10,
+    marginBottom: 20,
+    color:'white',
+    fontSize:28
+  },
+  buttonsContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    width: '100%'
   },
 
 });
