e0f9142b3aa228dcaa030292b550b03d100dfd60
Leonardo Rodrigues <49464793+leorodrigues311@users.noreply.github.com>
2024-12-10 11:38:26 -0300

    Comentarios na comandaDetalhe

--- app/comandaDetalhe.tsx
+++ app/comandaDetalhe.tsx
@@ -13,8 +13,8 @@ import * as Haptics from 'expo-haptics';
 export default function ComandaDetalhe () {
 
   const { nomeComanda, numeroComanda, horaAbertura, statusComanda } = useLocalSearchParams<ComandaDetalheParams>()
-  const router = useRouter()
-
+ 
+  //Este é o array dos itens da comanda
   const itensComanda = [   
     { numeroComanda: 1, nomeItem: 'Teste grelhado', valorUnit: 12.21, quantidade: 3 },
     { numeroComanda: 2, nomeItem: 'Pao com pao', valorUnit: 12.21, quantidade: 3 },
@@ -27,16 +27,16 @@ export default function ComandaDetalhe () {
     { numeroComanda: 9, nomeItem: 'Cuscuz', valorUnit: 12.21, quantidade: 3 }
   ]
 
-  const [selectedItems, setSelectedItems] = useState<number[]>([])  // Lista de itens selecionados
+  // Aqui fica o array dos itens que são selecionados quando você pressiona e segura um item
+  const [selectedItems, setSelectedItems] = useState<number[]>([])
 
-    // Função para limpar selectedItems
-    const limparSelecao = () => {
-      setSelectedItems([]); // Limpa o array
-    }
+  // Limpa o array (é acionado com a seta pra baixo da bottomBar)
+  const limparSelecao = () => {
+    setSelectedItems([])
+  }
 
-
+  // Quando aperta e segura em um item da comanda, ele vê se o array não tem nada, se o array estiver vazio, ele inclui no array
   const handleLongPress = (numeroComanda: number) => {
-    // A primeira vez que um item é pressionado por longo tempo
     if (selectedItems.length === 0) {
       Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Heavy)
     }
@@ -49,6 +49,7 @@ export default function ComandaDetalhe () {
     )
   }
 
+  // Este só inicia quando o handLongPress é feito uma primeira vez
   const handlePress = (numeroComanda: number) => {
     // Quando mais de um item é selecionado, usamos o onPress
     if (selectedItems.length !== 0) {
@@ -61,24 +62,29 @@ export default function ComandaDetalhe () {
     }
   }
 
+  // Verifica se a bottom bar está visível, através do array de selected items
   const isBottomBarVisible = selectedItems.length > 0;
 
 
   return (
     <SafeAreaView style={styles.viewPrincipal}>
 
+    {/*O hide Icons serve para remover os icones do TopBar, ele é acionado assim que a bottom bar fica visivel*/}
       <TopBarDetalheComanda hideIcons={isBottomBarVisible}/>
 
+    {/*Aqui fica o visualizador do status da comanda, estiver ativo, fica verde, inativo fica vermelho*/}
       <View style={styles.viewInfoComanda}>
         <View style={[styles.viewStatus, 
           { backgroundColor: statusComanda === 'ativo' ? '#00FF00' : '#FF0000' },
           { borderColor: statusComanda === 'ativo' ? '#00FF00' : '#FF0000' }
         ]}></View>
 
+    {/*Essa é a view que exibe o numero da comanda*/}
         <View style={styles.viewNumero}>
           <Text style={styles.viewNumeroTexto}>{numeroComanda}</Text>
         </View>
-
+    
+    {/*Essa é a view que traz os detalhes da comanda*/}
         <View style={styles.viewInfo}>
           <Text style={styles.viewInfoNome}>{nomeComanda}</Text>
           <Text style={styles.viewInfoHora}>Hora de Abertura: {horaAbertura}</Text>
@@ -87,6 +93,7 @@ export default function ComandaDetalhe () {
       </View>
 
       <ScrollView>
+        {/*Aqui nós percorremos o array itensComanda, para trazer todos os itens daquela comanda*/}
         <View style={styles.itensComanda}>
           {itensComanda.map((item) => (
             <Pressable
@@ -106,7 +113,10 @@ export default function ComandaDetalhe () {
         </View>
       </ScrollView>
 
+    {/*Se a bottomBar não estiver visivel, ele exibe o valor total*/}
       {(!isBottomBarVisible) && <ValorTotalComanda valorTotal={134.21}/>}
+
+    {/*Se a bottomBar estiver visivel, ele traz o elemento. Também passa o numero de itens que está selecionado através da prop, e a função de limpar seleção também*/}
       {isBottomBarVisible && <BottomBarDetalheComanda selectedItemsLength={selectedItems.length} limparSelecao={limparSelecao}/>}
       
     </SafeAreaView>
@@ -199,10 +209,5 @@ const styles = StyleSheet.create({
     borderColor:'#A9A9A9',
     borderWidth:0.6
   },
-
-  topBarPreta: {
-    backgroundColor: '#000000',  // Torna a TopBar preta
-    zIndex:0
-  }
 
 });
