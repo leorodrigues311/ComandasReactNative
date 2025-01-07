6820834dba8deee971b01ec8b577f22166b0b0df
Leonardo Rodrigues <49464793+leorodrigues311@users.noreply.github.com>
2025-01-07 12:12:12 -0300

    inclusão do botão e a lista de produtos adicionados

---
+++ components/ItemConferenciaAdd.tsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, Pressable, Animated, Dimensions } from 'react-native';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import * as Haptics from 'expo-haptics';
+
+export function ItemConferenciaAdd (){
+
+
+    return (
+        <View style = {styles.viewPrincipal}>
+            <Ionicons name="close-circle-outline" color={'red'} size={24}></Ionicons>
+            <Text style={styles.textoItem}>1 - Coca cola 2 Litros</Text>
+        </View>
+
+
+
+
+
+
+
+    );
+
+}
+
+const styles = StyleSheet.create({
+
+    viewPrincipal:{
+
+        flexDirection:'row'
+    },
+
+
+    textoItem:{
+
+        color:'white',
+        fontSize:18,
+        marginBottom:18,
+        marginLeft:5
+
+    }
+
+})
--- components/navigation/BottomBarConferirItens.tsx
+++ components/navigation/BottomBarConferirItens.tsx
@@ -2,13 +2,44 @@ import React, { useState } from 'react';
 import { View, Text, StyleSheet, Pressable, Animated, Dimensions } from 'react-native';
 import Ionicons from '@expo/vector-icons/Ionicons';
 import { SafeAreaView } from 'react-native-safe-area-context'; // Import SafeAreaView
+import { useRouter } from 'expo-router'
+import Dialog from "react-native-dialog"
+import { ItemConferenciaAdd } from '@/components/ItemConferenciaAdd'
 import * as Haptics from 'expo-haptics';
 
 export function BottomBarConferirItens({ selectedItemsLength, limparSelecao }: { selectedItemsLength: number, limparSelecao: () => void }) {
+
+  const router = useRouter()
   const screenHeight = Dimensions.get('window').height; // Altura total da tela
   const [isExpanded, setIsExpanded] = useState(false);
   const heightAnim = useState(new Animated.Value(60))[0]; // Animação para a altura
 
+  // Aqui nós exibimos o 'dialog'
+  const [dialogActionVisible, setDialogNovoProdutoVisible] = useState(false)
+
+  // Este cancela a ação do dialogo
+  const handleCancel = () => {
+    setDialogNovoProdutoVisible(false)
+  }
+
+  const handleFeedbackButton = () => {
+    Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Rigid);
+  }
+
+  const handleConfirmaInclusao = () => {
+    Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Rigid);
+    setDialogNovoProdutoVisible(true)
+  }
+
+  const adicionaItensComanda = () => {
+    setDialogNovoProdutoVisible(false)
+
+    router.back()
+
+
+  }
+
+ 
   const handleToggle = () => {
     Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Rigid);
 
@@ -42,9 +73,28 @@ export function BottomBarConferirItens({ selectedItemsLength, limparSelecao }: {
         {/* View extra que aparece ao expandir */}
         {isExpanded && (
           <View style={styles.viewExtra}>
-            <Text style={styles.textoViewExtra}>Os produtos escolhidos ficarão aqui</Text>
+            <ItemConferenciaAdd/>
+            <ItemConferenciaAdd/>
+            <ItemConferenciaAdd/>
+            <ItemConferenciaAdd/>
+            <ItemConferenciaAdd/>
+            <ItemConferenciaAdd/>
           </View>
+          
         )}
+
+        <Pressable onPressIn={handleFeedbackButton} onPressOut={handleConfirmaInclusao} style={styles.btnIncluir}>
+          <Text style={styles.textoBtnIncluir}>Incluir</Text>
+          <Ionicons name="checkbox-outline" size={38} color="white"/>
+        </Pressable>
+
+        <Dialog.Container visible={dialogActionVisible}>
+          <Dialog.Title>Adicionar Itens</Dialog.Title>
+          <Dialog.Description>Deseja Realmente Adicionar os itens à Comanda?</Dialog.Description>
+          <Dialog.Button onPress={handleCancel} label="Não" />
+          <Dialog.Button onPress={() => adicionaItensComanda()} label="Sim" />
+        </Dialog.Container>
+        
       </Animated.View>
   );
 }
@@ -83,15 +133,33 @@ const styles = StyleSheet.create({
   viewExtra: {
     backgroundColor: '#202122',
     flex: 1, // Preenche o restante do espaço disponível
-    padding: 10,
+    padding: 30,
     borderTopWidth: 1,
     borderTopColor: '#363636',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: 'flex-start',
+    justifyContent: 'flex-start',
   },
 
   textoViewExtra: {
     color: 'white',
     fontSize: 16,
   },
+
+  btnIncluir:{
+    height: 60,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingHorizontal: 15,
+    zIndex:1,
+    backgroundColor:'#02bf02',
+    borderRadius:8
+    
+  },
+
+  textoBtnIncluir:{
+    color: 'white',
+    fontSize: 28,
+
+  }
 });
